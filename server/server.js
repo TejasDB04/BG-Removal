96232181d10415a8d561a145600e4cfbf0d11f70
TejasDB <tejasdb05@gmail.com>
2024-11-15 23:07:33 +0530

    Webhooks Added

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import 'dotenv/config'
 import express from 'express'
 import cors from 'cors'
 import connectDB from './configs/mongodb.js'
+import userRouter from './routes/userRoutes.js'
 
 
 // App Config
@@ -17,4 +18,6 @@ app.use(cors())
 // API Routes
 
 app.get('/',(req,res)=> res.send("API Working"))
+app.use('/api/user',userRouter)
+
 app.listen(PORT, ()=> console.log("Server Running on port"+PORT))
