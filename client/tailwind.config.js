1533cfcf8293224b08cf66d8e5f6164f8d666f1b
TejasDB <tejasdb05@gmail.com>
2024-11-15 18:47:32 +0530

    First Commit

---
+++ client/tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // Matches all .js/.jsx/.ts/.tsx files in the src folder
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
---
+++ server/controllers/userController.js
@@ -0,0 +1,6 @@
+// API Controller Function to manage Clerk User with data bases
+// http://localhost:4000/api/user/webhooks
+
+const clerkWebhooks = async(req,res)=>{
+    
+}
